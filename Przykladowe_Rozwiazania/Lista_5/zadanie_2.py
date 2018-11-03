a981d4f1f0807111945d52a3a44df3a39477ad87
daniel <daniel@test.pl>
2018-11-03 19:59:27 +0100

    Poziom - Zaawansowany

--- Przykladowe_Rozwiazania/Lista_5/zadanie_2.py
+++
@@ -1,8 +0,0 @@
-import ciag_arytmetyczny
-
-a1 = input("Podaj a1: ")
-r = input("Podaj r: ")
-n = input("Podaj n: ")
-
-print "N-ty wyraz ciagu arytmetycznego: {}".format(ciag_arytmetyczny.nty_wyraz(a1,r,n))
-print "Suma N-tych wyrazow ciagu arytmetycznego: {}".format(ciag_arytmetyczny.suma_ntych_wyrazow(a1,r,n))
--- Python - Zaawansowany/Przykladowe_Rozwiazania/Lista_7/zadanie_3.py
+++ Python - Zaawansowany/Przykladowe_Rozwiazania/Lista_7/zadanie_3.py
@@ -7,4 +7,4 @@ for celcius_temp in celsius_temperatures:
     fahrenheit_temp = zadanie_1.convert_celsius_to_fahrenheit(float(celcius_temp))
     fahrenheit_temperatures.append(fahrenheit_temp)
 
-zadanie_1.save_to_file(fahrenheit_temperatures,"Fahrenheit.txt")
+zadanie_1.save_to_file(fahrenheit_temperatures, "Fahrenheit.txt")
